5840e7dd12d915feb8d05efba35cb2c382a52b6b
Anna Tolstova <anna_tolstova@hotmail.com>
2021-04-18 23:33:50 +0300

    feat: change refresh token type

--- src/resources/token/token.router.js
+++ src/resources/token/token.router.js
@@ -3,7 +3,7 @@ const router = require('express').Router({ mergeParams: true });
 
 const tokenService = require('./token.service');
 
-router.get('/', async (req, res) => {
+router.put('/', async (req, res) => {
   const tokens = await tokenService.refresh(req.userId, req.tokenId);
   res.status(OK).send(tokens);
 });
